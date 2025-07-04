228dc9da38bffd1a631f7097b8028b5a3607713e
DeMartiniAntonio <antonio.sluzbeni@gmail.com>
2025-07-04 11:32:06 +0200

    gameplay design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,25 +12,34 @@ public class GameManager : MonoBehaviour
     //population, wood, gold, food, stone, iron, tools,
     [Header("RES")]
     [SerializeField] private int population;
-    [SerializeField] private int workers;
-    [SerializeField] private int unemployed;
-    [SerializeField] private int wood;
+    [SerializeField] private int workers;       //rjeseno
+    [SerializeField] private int unemployed;    //rjeseno
+    [SerializeField] private int wood;          //rjeseno
     [SerializeField] private int gold;
-    [SerializeField] private int food;
+    [SerializeField] private int food;          //rjeseno
     [SerializeField] private int stone;
     [SerializeField] private int iron;
     [SerializeField] private int tools;
-    [SerializeField] private int days;
+    [SerializeField] private int days;          //rjeseno
     [Header("Buildings")]
     //farm, house, iron mines, gold mines, woodcutter, blacksmith, quarry
-    [SerializeField] private int farm;
-    [SerializeField] private int house;         //1house = 4ppl
+    [SerializeField] private int farm;          //rjeseno
+    [SerializeField] private int house;         //1house = 4ppl //rjeseno
     [SerializeField] private int ironMines;
     [SerializeField] private int goldMines;
-    [SerializeField] private int woodcutter;
+    [SerializeField] private int woodcutter;    //rjeseno
     [SerializeField] private int blacksmith;
     [SerializeField] private int quarry;
-
+    [Header("Resource text")]
+    [SerializeField] private TMP_Text populationText;
+    [SerializeField] private TMP_Text daysText;
+    [SerializeField] private TMP_Text woodText;
+    [SerializeField] private TMP_Text foodText;
+    [SerializeField] private TMP_Text ironText;
+    [Header("Building text")]
+    [SerializeField] private TMP_Text farmText;
+    [SerializeField] private TMP_Text woodcutterText;
+    [SerializeField] private TMP_Text housesText;
 
     private float timer;
 
@@ -41,7 +50,6 @@ public class GameManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Pause();
-
         }
     }
 
@@ -52,7 +60,10 @@ public class GameManager : MonoBehaviour
         if (timer >= 60)
         {
             days++;
-            GoodConsuprion(Population());
+            FoodGathering();
+            FoodProductions();
+            WoodProduction();
+            FoodConsuprion(1);
             IncreasePopulation();
             timer = 0;
         }
@@ -72,9 +83,15 @@ public class GameManager : MonoBehaviour
         return workers + unemployed;
     }
 
-    private void GoodConsuprion(int foodConsumed) {
+    private void FoodConsuprion(int foodConsumed) {
 
         food -= foodConsumed * Population();
+    }
+
+    private void FoodProductions()
+    {
+
+        food += farm * 4;
     }
 
     //number of max house * 4
@@ -84,20 +101,76 @@ public class GameManager : MonoBehaviour
         return maxPopulation;
     }
 
-    private void FoodProduction() 
+    private void FoodGathering() 
     {
         food += unemployed / 2;    
     }
 
     public void BuilldFarm() {
-        farm++;
-        unemployed--;
-        workers++;
+        if (wood >= 10 && CanAssignWorker(2))
+        {
+            wood -= 10;
+            farm++;
+            WorkerAssign(2);
+        }
+        else if(wood < 10)
+        {
+            Debug.Log($"Not enough resources to build Farm, you need {10 - wood} wood");
+        }
+        else if (!CanAssignWorker(2))
+        {
+            Debug.Log($"Not enough resources to build Farm, you need {2-unemployed} workers");
+        }
     }
 
+    public void BuilldWoodcutter()
+    {
+        if (wood >= 5 && iron > 0 && CanAssignWorker(1)) 
+        {
+            wood -= 5;
+            iron--;
+            woodcutter++;
+            WorkerAssign(1);
+        }
+        else if (wood < 5)
+        {
+            Debug.Log($"Not enough resources to build Farm, you need {10 - wood} wood");
+        }
+        else if (iron == 0)
+        {
+            Debug.Log($"Not enough resources to build Farm, you need 1 iron");
+        }
+        else if (!CanAssignWorker(1))
+        {
+            Debug.Log($"Not enough resources to build Farm, you need 1 worker");
+        }
+    }
 
+    public void WoodProduction()
+    {
+        wood += woodcutter * 2;
+    }
 
+    //TODO: make this method a class
+    private void BuildCost(int woodCost, int stoneCost, int workerAssign) {
 
+        if (wood >= woodCost && stone >= stoneCost && unemployed >= workerAssign)
+        {
+            wood -=woodCost;
+            stone -= stoneCost;
+            workers += workerAssign;
+        }
+    }
+
+    private void WorkerAssign(int amount) 
+    {
+        unemployed -= amount;
+        workers += amount;
+    }
+
+    private bool CanAssignWorker(int amount) {
+        return unemployed >= amount;
+    }
 
 
 
